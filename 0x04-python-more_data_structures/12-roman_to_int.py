3b02087087253b729444db76a073bd64a80ba9dd
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-11-08 15:44:04 -0800

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,10 +9,15 @@ def roman_to_int(roman_string):
     }
 
     result = 0
-    value = 0
+    prev_value = 0
 
     for char in reversed(roman_string):
-        value += roman_values[char]
-        result += value if result < value * 5 else -value
+        value = roman_values[char]
+
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
 
     return (result)
