f628f1f934c434d92dac2f15d2ab2aba04b6fc42
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-11-20 16:23:36 -0800

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except (ZeroDivisionError, ValueError):
+        resutl = None
+    finally:
+        print("Inside result: {:d}".format(result))
+        return result
