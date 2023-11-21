7aa4c50977356462f6ea0fbd8e6e6a5222115e52
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-11-21 14:38:42 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Module"""
+
+
+class Square:
+    """Define Square Class"""
+    def __init__(self, size):
+        """Define Constructor
+
+        Args:
+            size: The Size of Square
+        """
+        self.__size = size
