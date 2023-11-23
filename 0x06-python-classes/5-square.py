1bbcf1ab0d4767f8e75e0e0102a1ca7873d8e67b
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-11-22 17:51:24 -0800

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Class Module."""
+
+
+class Square:
+    """Defining Class."""
+
+
+    def __init__(self, size=0):
+        """Class Constructor.
+
+        Args:
+            size: Length of Square Side.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for size.
+
+        Raises:
+            TypeError: If Not Int.
+            ValueError: If Lower Than Zero.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """Area Function.
+
+        Return:
+            The Area of Square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print Square."""
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 else "")
+        print()
+
