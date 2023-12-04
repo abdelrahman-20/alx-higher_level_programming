c4f2eec6642c6c90c0cfcdc6a1f13b63f77bccc8
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-04 15:04:42 -0800

    Task 10

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,24 @@
-#!/usr/bin/python3
-'''Module For Rectangle Class.'''
-
+# !/usr/bin/python3
+"""Module For Rectangle Class."""
 BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-	'''A subclass representing rectangle.'''
+    """A subclass representing rectangle."""
 
-	def __init__(self, width, height):
-		'''Constructor.'''
-		self.integer_validator("width", width)
-		self._width = width
-		self.integer_validator("height", height)
-		self._height = height
+    def __init__(self, width, height):
+        """Constructor."""
+        self.integer_validator("width", width)
+        self._width = width
+        self.integer_validator("height", height)
+        self._height = height
 
-	def area(self):
-		'''Returns The Area of The Rectangle.'''
-		return self.__width * self.__height
+    def area(self):
+        """Returns The Area of The Rectangle."""
+        return self.__width * self.__height
 
-        def __str__(self):
-		'''Print in Specific Way.'''
-		string = "[" + str(self.__class__.__name__) + "]"
-		string += str(self.__width) + "/" + str(self.__height)
-		return string
+    def __str__(self):
+        """Print in Specific Way."""
+        string = "[" + str(self.__class__.__name__) + "]"
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
