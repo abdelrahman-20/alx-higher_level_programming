68d95b59503efa572f135bcb59d9064a2e724491
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-04 14:09:46 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module For A Method."""
+
+
+class BaseGeometry:
+    """This is Empty Class."""
+    def area(self):
+        """A Function To Get The Area and Not Implemented Yet."""
+        raise Exception("area() is not implemented")
