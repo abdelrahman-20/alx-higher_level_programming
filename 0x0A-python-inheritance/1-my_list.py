ef3e4f296ab32ed06b125a31e69de2fa80779ecc
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-04 13:07:13 -0800

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module For A Method."""
+
+
+class MyList(list):
+    """A List Class."""
+
+    def print_sort(self):
+        """A Function To Print A Sorted List."""
+        print(sorted(self))
