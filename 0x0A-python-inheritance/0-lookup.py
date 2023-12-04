3e23e115f7ded41cc337ae2396ae88e8aa776334
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-04 12:58:13 -0800

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module For A Method."""
+
+
+def lookup(obj):
+    """A Function To Look Up An Object.
+
+    Args:
+        obj: The Object.
+
+    Returns:
+        A List of Attributes.
+    """
+
+    return dir(obj)
