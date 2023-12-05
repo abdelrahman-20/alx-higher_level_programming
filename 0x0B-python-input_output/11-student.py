ff345aa780dbda1ad20aaaffd2e1b6689b98cb75
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-05 11:46:03 -0800

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Class Student Module."""
+
+
+class Student:
+    """Student Class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Class Constructor."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary representation.
+
+        Args:
+            attrs: The attributes to get.
+
+            Returns a dictionary representation.
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+
+        return my_dict
+
+    def reload_from_json(self, json):
+        """A function to modify the json representation."""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
