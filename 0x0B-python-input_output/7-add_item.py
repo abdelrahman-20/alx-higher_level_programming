917bdcceef3241598e4c57b583c481c6e6f01e42
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-05 02:58:13 -0800

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Input and output module."""
-
-
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-arg_list = list(sys.argv[1:])
 
-try:
-    old_data = list(load_from_json_file('add_item.json'))
-except Exception:
-    old_data = []
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__(
+        "6-load_from_json_file").load_from_json_file
 
-old_data.extend.extend(arg_list)
-save_to_json_file(old_data, "add_item.json")
+    arg_list = list(sys.argv[1:])
+
+    try:
+        old_data = load_from_json_file('add_item.json')
+    except Exception:
+        old_data = []
+
+    old_data.extend.extend(arg_list)
+    save_to_json_file(old_data, "add_item.json")
