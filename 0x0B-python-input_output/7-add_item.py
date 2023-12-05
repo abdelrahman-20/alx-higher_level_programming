76c4e0b59724331d597c0ada04ef232e7ffa12f4
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-05 02:52:36 -0800

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 arg_list = list(sys.argv[1:])
 
 try:
-    old_data = load_from_json_file('add_item.json')
+    old_data = list(load_from_json_file('add_item.json'))
 except Exception:
     old_data = []
 
