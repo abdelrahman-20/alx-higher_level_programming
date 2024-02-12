7a9f25acd8e382cfb624d78a6f929afff4252aad
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-02-12 04:02:53 -0800

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,10 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   const x = process.argv[2];
+  let i = 0;
 
-  while (x--) {
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
