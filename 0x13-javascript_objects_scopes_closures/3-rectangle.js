3626ea70491dc65b45275c41b6b6d37177210f32
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-06-19 17:06:41 -0700

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,14 +8,9 @@ class Rectangle {
     }
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
-    }
+  print() {
+    for (let h = 0; h < this.height; h++)
+      console.log('X'.repeat(this.width));
   }
 }
 
