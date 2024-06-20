9a4e89a821658e06486fded9788f8cef971cd145
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-06-19 17:40:21 -0700

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@ exports.esrever = function (list) {
   const n = list.length;
 
   for (let i = 0; i < n / 2; i++) {
-    let temp = list[i];
+    const temp = list[i];
     list[i] = list[n - i - 1];
     list[n - i - 1] = temp;
   }
