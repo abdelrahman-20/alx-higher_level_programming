1dcf4bb7a03f98be198dc994121fdf61a45a37fd
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-07-05 15:45:59 -0700

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Handling HTTP Error"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error Code: {}".format(err.status))
